fd8b1ed10467ff29b715ab6258e6c69b3d4f1587
xiezefan <xiezefan@126.com>
2014-11-03 23:05:09 +0800

    fix wechatdemo

---
+++ android/WeChatDemo/app/src/main/java/org/easyim/wechatdemo/app/FoundFragment.java
@@ -0,0 +1,34 @@
+package org.easyim.wechatdemo.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+/**
+ * Created by xiezefan-pc on 2014/11/1.
+ */
+public class FoundFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        FrameLayout fl = new FrameLayout(getActivity());
+        fl.setLayoutParams(params);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, dm);
+        TextView v = new TextView(getActivity());
+        params.setMargins(margin, margin, margin, margin);
+        v.setLayoutParams(params);
+        v.setGravity(Gravity.CENTER);
+        v.setText("发现界面");
+        v.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, dm));
+        fl.addView(v);
+        return fl;
+    }
+}
--- android/WeChatDemo/app/src/main/java/org/easyim/wechatdemo/app/MainActivity.java
+++ android/WeChatDemo/app/src/main/java/org/easyim/wechatdemo/app/MainActivity.java
@@ -1,20 +1,105 @@
 package org.easyim.wechatdemo.app;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
 import android.view.Window;
+import com.easyim.com.astuetz.wechat.PagerSlidingTabStrip;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ChatFragment chatFragment;
+    private FoundFragment foundFragment;
+    private ContactsFragment contactsFragment;
+
+    private PagerSlidingTabStrip tabs;
+
+    private DisplayMetrics dm;
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setOverflowShowingAlways();
+        dm = getResources().getDisplayMetrics();
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        Log.d(TAG, (tabs == null) + "");
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        tabs.setViewPager(pager);
+        setTabsViews();
+
+    }
+
+    private void setTabsViews() {
+        tabs.setShouldExpand(true);
+        tabs.setDividerColor(Color.TRANSPARENT);
+        tabs.setUnderlineHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, dm));
+        tabs.setIndicatorHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, dm));
+        tabs.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, dm));
+        tabs.setTabBackground(0);
+        tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+        tabs.setSelectedTabTextColor(Color.parseColor("#45c01a"));
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+        private final String[] titles = {"聊天", "发现", "通讯录"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    if (chatFragment == null) {
+                        chatFragment = new ChatFragment();
+                    }
+                    return chatFragment;
+                case 1:
+                    if (contactsFragment == null) {
+                        contactsFragment = new ContactsFragment();
+                    }
+                    return contactsFragment;
+                case 2:
+                    if (foundFragment == null) {
+                        foundFragment = new FoundFragment();
+                    }
+                    return foundFragment;
+                default:
+                    Log.e(TAG, "[getItem] Invalid position:" + position);
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
     }
 
 
@@ -52,5 +137,16 @@ public class MainActivity extends Activity {
             }
         }
         return super.onMenuOpened(featureId, menu);
+    }
+
+    public void setOverflowShowingAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            //ignore
+        }
     }
 }
